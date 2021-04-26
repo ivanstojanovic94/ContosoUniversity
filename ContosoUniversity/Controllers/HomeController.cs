b5875dbcdbc7002099dc5486a00943baf919f70d
ivanstojanovic <stojanovicivan717@gmail.com>
2021-04-26 12:54:16 +0200

    Add project files.

---
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.VewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+      private SchoolContext db = new SchoolContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dategroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dategroup.Key,
+                                                       StudentCount = dategroup.Count()
+                                                   };
+
+            return View(data.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disponsing)
+        {
+            db.Dispose();
+            base.Dispose(disponsing);
+        }
+    }
+}
