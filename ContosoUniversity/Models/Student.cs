6c46df25c15c08e4c8cb5b743de0391c21001c0f
ivanstojanovic <stojanovicivan717@gmail.com>
2021-05-06 11:07:21 +0200

    solution connected to real database

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,7 +9,7 @@ namespace ContosoUniversity.Models
 {
     public class Student
     {
-        public int ID { get; set; }
+        public int StudentID { get; set; }
 
         [Required]
         [StringLength(50)]
